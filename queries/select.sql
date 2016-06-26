260b7353ded3a72dca27d316c57aaee6610f6d27
shigemk2 <i.am.shige@gmail.com>
2016-06-27 00:52:13 +0900

    Example: treasure hive

---
+++ queries/select.sql
@@ -0,0 +1,1 @@
+SELECT code, COUNT(1) AS cnt FROM www_access GROUP BY code
--- tasks/__init__.py
+++ tasks/__init__.py
@@ -1,8 +1,11 @@
+import digdag
+import os
 
 class MyWorkflow(object):
     def __init__(self):
         pass
 
-    def step3(self, session_time = None):
-        print("Step3 of session {0}".format(session_time))
+    def loadEnv(self, session_time = None):
+        digdag.env.store({"AWS_ACCESS_KEY":os.environ.get("AWS_ACCESS_KEY")})
+        digdag.env.store({"AWS_SECRET_KEY":os.environ.get("AWS_SECRET_KEY")})
 
