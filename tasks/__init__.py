7d55dc46134f72d21a681a9300cf9aa5e7de244f
shigemk2 <i.am.shige@gmail.com>
2016-06-28 00:32:47 +0900

    Fix environment variable

--- tasks/__init__.py
+++ tasks/__init__.py
@@ -6,6 +6,6 @@ class MyWorkflow(object):
         pass
 
     def loadEnv(self, session_time = None):
-        digdag.env.store({"AWS_ACCESS_KEY":os.environ.get("AWS_ACCESS_KEY")})
-        digdag.env.store({"AWS_SECRET_KEY":os.environ.get("AWS_SECRET_KEY")})
+        digdag.env.store({"AWS_ACCESS_KEY":os.environ.get("AWS_ACCESS_KEY_ID")})
+        digdag.env.store({"AWS_SECRET_KEY":os.environ.get("AWS_SECRET_ACCESS_KEY")})
 
